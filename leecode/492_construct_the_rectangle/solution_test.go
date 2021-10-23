1441adda35195da16f16871b318d5ffdbd763c52
genox <genius840215@gmail.com>
2021-10-23 17:57:27 +0800

    feat: add LeeCode solution (492)

---
+++ leecode/492_construct_the_rectangle/solution_test.go
@@ -0,0 +1,30 @@
+package _92_construct_the_rectangle
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_constructRectangle(t *testing.T) {
+	type args struct {
+		area int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			"test case 1",
+			args{4},
+			[]int{2, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := constructRectangle(tt.args.area); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("constructRectangle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
