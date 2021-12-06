c930d63437dba6ce187bcc49778cd5eee173cca1
genox <genius840215@gmail.com>
2021-12-06 09:24:30 +0800

    feat: add LeeCode solution (1816)

---
+++ leecode/1816_truncate_sentence/solution_test.go
@@ -0,0 +1,45 @@
+package _816_truncate_sentence
+
+import "testing"
+
+func Test_truncateSentence(t *testing.T) {
+	type args struct {
+		s string
+		k int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"test case 1",
+			args{
+				"Hello how are you Contestant",
+				4,
+			},
+			"Hello how are you",
+		}, {
+			"test case 2",
+			args{
+				"What is the solution to this problem",
+				4,
+			},
+			"What is the solution",
+		}, {
+			"test case 3",
+			args{
+				"chopper is not a tanuki",
+				5,
+			},
+			"chopper is not a tanuki",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := truncateSentence(tt.args.s, tt.args.k); got != tt.want {
+				t.Errorf("truncateSentence() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
