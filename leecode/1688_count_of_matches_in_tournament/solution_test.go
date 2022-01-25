8e7f9e52c75319a5f0cb46850369daafe88c5c9b
genox <genius840215@gmail.com>
2022-01-25 08:34:40 +0800

    feat: add LeeCode solution (1220)

---
+++ leecode/1688_count_of_matches_in_tournament/solution_test.go
@@ -0,0 +1,31 @@
+package _688_count_of_matches_in_tournament
+
+import "testing"
+
+func Test_numberOfMatches(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"test case 1",
+			args{7},
+			6,
+		}, {
+			"test case 2",
+			args{14},
+			13,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := numberOfMatches(tt.args.n); got != tt.want {
+				t.Errorf("numberOfMatches() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
